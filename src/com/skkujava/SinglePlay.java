88ad3e2c20729d120e4c88fc3d1b8d8da438b78e
Jiwon-Park <wldnjs1323@naver.com>
2019-05-24 14:18:49 +0900

    0524. 14:18

--- src/com/skkujava/SinglePlay.java
+++ src/com/skkujava/SinglePlay.java
@@ -62,16 +62,88 @@ public class SinglePlay extends Game {
     }
 
     private void GetReward(){
-        System.out.println("1: 카드를 한 장 덱에 추가합니다\n2: 덱에서 카드를 한 장 뺍니다\n3: 체력을 모두 회복합니다.(이 보상을 고르지 않아도 1/3의 체력이 회복됩니다.)");
+        boolean flag = true;
+        System.out.println("1: 카드를 한 장 덱에 추가합니다\n2: 덱에서 카드를 한 장 뺍니다\n3: 체력을 모두 회복합니다.(이 보상을 고르지 않아도 1/3의 체력이 회복됩니다.)\n4: 카드를 한 장 강화합니다.");
         String userInput = scanner.next();
         do {
             if (userInput.trim().equals("1")) {
                 break;
             } else if (userInput.trim().equals("2")) {
+                Card card;
+                do {
+                    System.out.println("제거할 카드를 선택하세요");
+                    for (int i = 0; i < player.grave.size(); i++) {
+                        card = player.grave.get(i);
+                        System.out.printf("%d: %-7s│%s", i + 1, card.name, card.cardDescription());
+                    }
+                    int inp;
+                    do {
+                        inp = scanner.nextInt();
+                        if (inp <= 0 || inp > player.deck.size()) {
+                            System.out.println("Invalid input! Please re-input!");
+                        } else break;
+                    } while (true);
+
+                    card = player.grave.get(inp);
+
+                    System.out.println("다음 카드를 제거하시겠습니까? (Y/N)");
+                    System.out.printf("%-7s│%s\n", card.name, card.cardDescription());
+                    
+                    do{
+                        userInput = scanner.next();
+                        if(userInput.equals("Y")){
+                            player.grave.remove(inp);
+                            flag = false;
+                            break;
+                        } else if(userInput.equals("N")){
+                            break;
+                        } else {
+                            System.out.println("Invalid input! Please re-input!");
+                        }
+                    } while(true);
+
+                }while(flag);
+
                 break;
             } else if (userInput.trim().equals("3")) {
                 player.setHp(player.getMaxhp());
                 break;
+            } else if (userInput.trim().equals("4")) {
+                Card card;
+                do {
+                    System.out.println("강화할 카드를 선택하세요");
+                    for (int i = 0; i < player.grave.size(); i++) {
+                        card = player.grave.get(i);
+                        System.out.printf("%d: %-7s│%s", i + 1, card.name, card.cardDescription());
+                    }
+                    int inp;
+                    do {
+                        inp = scanner.nextInt();
+                        if (inp <= 0 || inp > player.deck.size()) {
+                            System.out.println("Invalid input! Please re-input!");
+                        } else break;
+                    } while (true);
+
+                    card = player.grave.get(inp);
+
+
+                    //TODO: make clone
+                    System.out.println("다음 카드를 강화하시겠습니까? (Y/N)");
+                    System.out.printf("%-7s│%s\n%-15s\n%-7s│%s\n", card.name, card.cardDescription(), "↓");
+
+                    do{
+                        userInput = scanner.next();
+                        if(userInput.equals("Y")){
+                            card.reinforce();
+                            flag = false;
+                            break;
+                        } else if(userInput.equals("N")){
+                            break;
+                        } else {
+                            System.out.println("Invalid input! Please re-input!");
+                        }
+                    } while(true);
+                }while(flag);
             } else {
                 System.out.println("Invalid input! Please re-input!");
             }
