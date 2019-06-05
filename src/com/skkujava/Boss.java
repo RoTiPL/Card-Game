261bc0b4f6b3a1ed880f3a570357163e1f1a40df
Kangdyu <blning1999@gmail.com>
2019-06-05 15:07:38 +0900

    0605 15:07 Upgrade Boss action / Change scripts to English

--- src/com/skkujava/Boss.java
+++ src/com/skkujava/Boss.java
@@ -52,7 +52,8 @@ class Poisoner extends Boss {
                 PoisonBottle();
                 break;
             case 1:
-                PoisonAmplification();
+                if(!player.isPoisoned()) PoisonBottle();
+                else                     PoisonAmplification();
                 break;
             case 2:
                 Defend();
@@ -62,30 +63,27 @@ class Poisoner extends Boss {
         if(player.isPoisoned()) {
             player.TakePoisonDamage();
         }
-
-        if(player.getHp() <= 0) {
-            // Game Over
-        }
     }
 
     public void PoisonBottle() {
         int damage = 5 + floor;
         player.setPoisoned(true);
         player.setPoisonDamage(player.getPoisonDamage() + damage);
-        System.out.println("독술사가 독병을 던집니다.");
-        System.out.println(damage + "만큼의 독에 중독되었습니다.");
+        System.out.println("Poisoner throws poison bottle!");
+        System.out.println("You got " + damage + " poison.");
     }
 
     public void PoisonAmplification() {
         player.setPoisonDamage(player.getPoisonDamage() * 2);
-        System.out.println("독술사가 당신의 독을 증폭시킵니다. 독이 두배로 증폭됩니다.");
+        System.out.println("Poisoner amplifies your poison!");
+        System.out.println("Your poison is amplified by x2.");
     }
 
     public void Defend() {
         int armor = 5 + floor;
         setArmor(getArmor() + armor);
-        System.out.println("독술사가 방어 자세를 취합니다.");
-        System.out.println("독술사가 " + armor + "의 방어도를 얻었습니다.");
+        System.out.println("Poisoner takes guard action!");
+        System.out.println("Poisoner got " + armor + " armor.");
     }
 }
 
@@ -118,32 +116,28 @@ class Giant extends Boss {
                 DoubleSwing();
                 break;
         }
-
-        if(player.getHp() <= 0) {
-            // Game Over
-        }
     }
 
     public void BigSwing() {
         int damage = 10 + 2 * floor + getStrength();
         player.TakeDamage(damage);
-        System.out.println("거인이 큰 방망이를 휘두릅니다.");
-        System.out.println(damage + "의 피해를 입었습니다.");
+        System.out.println("Giant swings his big bat!");
+        System.out.println("You got " + damage + " damage.");
     }
 
     public void OverflowingStrength() {
         setStrength(getStrength() + 3);
-        System.out.println("거인이 기합을 외칩니다.");
-        System.out.println("거인의 힘이 3만큼 증가합니다.");
+        System.out.println("Giant is shouting!");
+        System.out.println("Giant got 3 strength.");
     }
 
     public void DoubleSwing() {
         int damage = 5 + floor + getStrength();
         player.TakeDamage(damage);
         player.TakeDamage(damage);
-        System.out.println("거인이 방망이를 두 번 휘두릅니다.");
-        System.out.println(damage + "의 피해를 입었습니다.");
-        System.out.println(damage + "의 피해를 입었습니다.");
+        System.out.println("Giant swings his bat twice!");
+        System.out.println("You got " + damage + " damage.");
+        System.out.println("You got " + damage + " damage.");
     }
 }
 
@@ -170,23 +164,20 @@ class Slime extends Boss {
                 Attack();
                 break;
             case 1:
-                Heal();
+                if(getHp() == getMaxHp()) Defend();
+                else                      Heal();
                 break;
             case 2:
                 Defend();
                 break;
-        }
-
-        if(player.getHp() <= 0) {
-            // Game Over
         }
     }
 
     public void Attack() {
         int damage = 7 + floor + getStrength();
         player.TakeDamage(damage);
-        System.out.println("슬라임이 몸통박치기를 시전합니다.");
-        System.out.println(damage + "만큼의 피해를 입었습니다.");
+        System.out.println("Slime is rushing to you!");
+        System.out.println("You got " + damage + " damage.");
     }
 
     public void Heal() {
@@ -195,14 +186,14 @@ class Slime extends Boss {
             setHp(getMaxHp());
         else
             setHp(getHp() + heal);
-        System.out.println("슬라임이 자가 회복을 했습니다.");
-        System.out.println("슬라임이 " + heal + "만큼의 체력을 회복했습니다.");
+        System.out.println("Slime heals itself!");
+        System.out.println("Slime healed " + heal + " HP.");
     }
 
     public void Defend() {
         int armor = 5 + floor;
         setArmor(getArmor() + armor);
-        System.out.println("슬라임이 방어 자세를 취합니다.");
-        System.out.println("슬라임이 " + armor + "의 방어도를 얻었습니다.");
+        System.out.println("Slime takes guard action!");
+        System.out.println("Slime got " + armor + " armor.");
     }
 }
